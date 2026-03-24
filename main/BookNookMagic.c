b74f52dc07636e57ecb582128b91028964c6a872
mmiller <martinberuf@gmail.com>
2026-03-24 01:06:34 +0100

    flame is now animated

--- main/BookNookMagic.c
+++ main/BookNookMagic.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 #include "esp_err.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
 #include "esp_log.h"
+#include "esp_netif.h"
 #include "driver/gpio.h"
 #include "driver/spi_master.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "nvs_flash.h"
+#include "sdkconfig.h"
+
+
 
 #include "esp_lcd_panel_io.h"
 #include "esp_lcd_panel_ops.h"
@@ -22,9 +31,6 @@
 #include "FlameUnified8.h"
 #include "FlameUnified9.h"
 
-
-static const char *TAG = "tft";
-
 #define PIN_NUM_SCLK    18   // SCK
 #define PIN_NUM_MOSI    23   // SDA (MOSI)
 #define PIN_NUM_CS      5    // CS
@@ -36,6 +42,17 @@ static const char *TAG = "tft";
 #define TFT_V_RES       160
 
 #define FRAME_TIME_MS   200
+
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+#define MAXIMUM_RETRY      5
+
+static const char *TAG = "wifi station";
+static EventGroupHandle_t wifi_event_group;
+static int s_retry_num = 0;
+
+static const char *TAG = "tft";
+
 
 
 static void fill_solid_color(esp_lcd_panel_handle_t panel, uint16_t color_rgb565)
@@ -141,7 +158,104 @@ void presence_task(void *pvParameters)
     ESP_LOGI(TAG, "Presence Task started");
 }
 
+static void wifi_event_handler(void *arg,
+                               esp_event_base_t event_base,
+                               int32_t event_id,
+                               void *event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "Verbindungsversuch erneut: %d/%d", s_retry_num, MAXIMUM_RETRY);
+        } else {
+            xEventGroupSetBits(wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG, "Verbindung zum WLAN getrennt");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+void wifi_init_sta(void)
+{
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    wifi_config_t wifi_config = {0};
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+
+    wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    strncpy((char *)wifi_config.sta.ssid,
+            CONFIG_BOOKNOOK_WIFI_SSID,
+            sizeof(wifi_config.sta.ssid) - 1);
+
+    strncpy((char *)wifi_config.sta.password,
+            CONFIG_BOOKNOOK_WIFI_PASSWORD,
+            sizeof(wifi_config.sta.password) - 1);
+
+    wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+    wifi_config.sta.pmf_cfg.capable = true;
+    wifi_config.sta.pmf_cfg.required = false;
+
+    ESP_LOGI(TAG,
+             "Verbinde mit SSID:%s Passwort:%s",
+             CONFIG_BOOKNOOK_WIFI_SSID,
+             CONFIG_BOOKNOOK_WIFI_PASSWORD);
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    EventBits_t bits = xEventGroupWaitBits(wifi_event_group,
+                                           WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                           pdFALSE,
+                                           pdFALSE,
+                                           portMAX_DELAY);
+
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG,
+                 "connected to ap SSID:%s password:%s",
+                 CONFIG_BOOKNOOK_WIFI_SSID,
+                 CONFIG_BOOKNOOK_WIFI_PASSWORD);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG,
+                 "Verbindung fehlgeschlagen zu SSID:%s Passwort:%s",
+                 CONFIG_BOOKNOOK_WIFI_SSID,
+                 CONFIG_BOOKNOOK_WIFI_PASSWORD);
+    } else {
+        ESP_LOGE(TAG, "Unerwartetes Event");
+    }
+}
+
+
 void app_main(void) {
+
+    wifi_init_sta();
 
     xTaskCreate(animaltion_task, "animation_task", 4096, NULL, 5, NULL); // ( TaskName, TaskStackSize, TaskParameter, TaskPriority, TaskHandle
     xTaskCreate(presence_task, "presence_task", 4096, NULL, 5, NULL); // ( TaskName, TaskStackSize, TaskParameter, TaskPriority, TaskHandle
